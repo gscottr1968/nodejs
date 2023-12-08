c4951ef534eae84927c60c3fd5ef8413675692dc
Gordon Rowe <gscottr1968@gmail.com>
2023-12-08 13:41:19 -0500

    Update package.json

--- src/app.js
+++ src/app.js
@@ -1,4 +1,15 @@
-const { v4: uuidv4 }= require('uuid');
-// import { v4 as uuidv4 } from 'uuid'
+const express = require('express');
+const app = express();
+const PORT = 5500;
 
-console.log(uuidv4())
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+app.post('/', (req, res) => {
+    res.send("This is a post request")
+})
+
+app.listen(PORT, () => {
+    console.log('App listening on port ' + PORT);
+});
